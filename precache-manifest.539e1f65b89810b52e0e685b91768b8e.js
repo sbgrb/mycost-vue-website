ddc3382df9c1fa6b3105d1b183edd0cf04f569c4
sbgrb <974487599@qq.com>
2020-09-01 23:49:49 +0800

    deploy

---
+++ precache-manifest.539e1f65b89810b52e0e685b91768b8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f19ce69926fc4fab1a7d",
+    "url": "/mycost-vue-website/css/app.5447a9fb.css"
+  },
+  {
+    "revision": "67fa672b8fd577a7049d790364073592",
+    "url": "/mycost-vue-website/index.html"
+  },
+  {
+    "revision": "f19ce69926fc4fab1a7d",
+    "url": "/mycost-vue-website/js/app.13652eaf.js"
+  },
+  {
+    "revision": "c36006a12eefee9d6e7b",
+    "url": "/mycost-vue-website/js/chunk-vendors.49cccadd.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/mycost-vue-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mycost-vue-website/robots.txt"
+  }
+]);
